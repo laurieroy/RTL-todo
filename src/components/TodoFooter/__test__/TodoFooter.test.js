164d1e5f1bd92dbc5f679470da8886e8454659f8
Laurie Roy <laurieroy.dev@gmail.com>
2023-01-07 14:58:33 -0800

    Mock TodoFooter

---
+++ src/components/TodoFooter/__test__/TodoFooter.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from "react-router-dom";
+import TodoFooter from '../TodoFooter'
+
+const MockTodoFooter = ({numberOfIncompleteTasks}) => {
+  return (
+    <BrowserRouter>
+      <TodoFooter numberOfIncompleteTasks={numberOfIncompleteTasks} />
+    </BrowserRouter>
+  )
+}
+
+it('should render the correct amount of incomplete tasks', async () => {
+  render(
+  <MockTodoFooter numberOfIncompleteTasks={5} />);
+  const paragraphElement = screen.getByText(/5 tasks left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
+
+it('should render "task" for a single task', async () => {
+  render(<MockTodoFooter numberOfIncompleteTasks={1} />);
+  const paragraphElement = screen.getByText(/1 task left/i);
+  expect(paragraphElement).toBeInTheDocument();
+});
